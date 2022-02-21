6e5d95ba2167b9dd2056de8200441bfbb0b701d7
vadimPetru <6ddd14@gmail.com>
2022-02-21 16:34:47 +0300

    1

--- js/dom.js
+++ js/dom.js
@@ -1,87 +1,130 @@
 console.log("Javascript - лучший язык программирования!");
-window.addEventListener("load", function() {
+window.addEventListener("DOMContentLoaded", function () {
+  
+  //1. Поменяйте приветствие с "Привет Всем!" на "Привет Виталий Валентинович!".
   setTimeout(() => {
-   
-    // #1 I changed content greeting on "Привет Виталий Валентинович"
-    let editMessage = document.querySelector("#greeting");
-    editMessage.textContent = 'Привет Виталий Валентинович!';
+    let privet = document.querySelector("#greeting");
+    privet.textContent = 'Привет Виталий Валентинович!';
+  }, 2000);
 
-    // #2 I set background color for <li> on "yellow"
+//2. Установите фоновый цвет для каждого `<li>` на `yellow`.
+  setTimeout(() => {
     document.querySelector("#essentials").style.backgroundColor = "yellow";
-    
-    // #3 I created image's tag , Seted its attribute ,and enclosed it in a div with the id #greeting.
-    let appendChild = document.createElement("img");
-    appendChild.setAttribute("src", "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif");
-    document.querySelector("#greeting").appendChild(appendChild);  
-    
-    //#4  I created and added an ul element to the end of the body with the "todo-items" class
-    let uL = document.createElement("ul");
-    uL.classList.add ("todo-items");
-    document.querySelector("body").appendChild(uL);
+  }, 2200);
 
-    //#5  I went through the array ['make coffee','eat salad','change baby's diaper','go to work'] and created a li tag for each element, for example: <li>make coffee</li>
-    // #6 Add each previously created li tag to your ul construct with the "todo-items" class.
-    let array = ['приготовить кофе','съесть салатик','поменять подгузник ребёнку','поехать на работу'];
-    for (let i = 0; i < array.length; i++ ) {
-      let lI = document.createElement("li");
-      lI.textContent = array[i];
-      document.querySelector(".todo-items").appendChild(lI);
+//3. Создайте тег изображения, установите его атрибут `src` на`http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif`, и вложите в div с идентификатором `#greeting`.
+  setTimeout(() => {
+    let pictureNew = document.createElement("img");
+    pictureNew.setAttribute("src", "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif");
+    document.querySelector('#greeting').appendChild(pictureNew);
+  }, 2400);
+
+//4. Создайте и добавьте ul элемент в конец  body с классом "todo-items"
+  setTimeout(() => {
+    let ulNew = document.createElement("ul");
+    ulNew.classList.add("todo-items");
+    document.querySelector("body").appendChild(ulNew);
+  }, 2600);
+  
+//5. Пройдитесь по массиву `['приготовить кофе','съесть салатик','поменять подгузник ребёнку','поехать на работу']` и создайте тег li для каждого элемента, например: `<li>приготовить кофе</li>`
+    let arrayNew = [];
+  setTimeout(() => {
+    let list = ['приготовить кофе', 'съесть салатик', 'поменять подгузник ребёнку', 'поехать на работу'];
+    for (let i = 0; i < list.length; i++) {
+      let liNew = document.createElement('li');
+      liNew.textContent = list[i];
+      arrayNew[i] = liNew;
     }
-   
+  }, 2600);
 
-   //#7  I Added the selected class to <li> when anything click on it. Removed it from any other li.
-  //#8 Changed the image so that it is the last selected food item.
-   let Ul = document.querySelector("#spoky");
-    Ul.addEventListener("click",addClass, false);
-    function addClass(Class) {
-      const knots = Class.target.parentNode.childNodes;
-      knots.forEach(knot => {
-        if (knot.nodeName === 'LI') {
-          knot.classList.remove('selected')
-        }
+//6. Добавьте каждый созданный ранее тег li в вашу конструкцию ul с классом "todo-items".
+  setTimeout(() => {
+    let perenos = document.querySelector(".todo-items");
+    for (let i = 0; i < arrayNew.length; i++) {
+      perenos.appendChild(arrayNew[i]);
+    }
+  }, 2600);
+
+ // 7. Добавьте класс `selected` к `<li>` при щелчке по нём.Удалите его из любых других`li`.
+  setTimeout(() => {
+    let classNew = document.querySelector("#essentials")
+    classNew.addEventListener("click", function (e) {
+    let selectedNew = document.querySelectorAll('.selected')
+    let object = e.target
+    Array.from(selectedNew).forEach(selectedNew => {
+      selectedNew.classList.remove('selected')
+    });
+
+    object.classList.add('selected')
+    let imagesNew =
+    {
+      "молоко": "images/milk.jpg",
+      "мёд": "images/honey.jpg",
+      "вода": "images/water.jpg",
+      "вино": "images/wine.jpg",
+      "пиво": "images/beer.jpg",
+    }
+    let  title = object.innerText;
+    let images = document.querySelector("#logo");
+    images.setAttribute('src', imagesNew[title]);
+  });
+  }, 2800);
+
+ // 8. Измените изображение, чтобы оно было последним выбранным продуктом питания.
+  setTimeout(() => {
+     let change = document.querySelector(".todo-items")
+     change.addEventListener("click", function (e) {
+       let changeNew = document.querySelectorAll('.selected')
+       let objectNew = e.target
+       Array.from(changeNew).forEach(selectedTo => {
+         changeNew.classList.remove('selected')
+        });
+        objectNew.classList.add('selected')
       });
-     Class.target.classList.add('selected');
-     const key = Class.target.innerText;
-      let obj = {
-        "мёд": "images/honey.jpg",
-        "молоко": "images/milk.jpg",
-        "вода": "images/water.jpg",
-        "вино": "images/wine.jpg",
-        "пиво": "images/beer.jpg"
-      }
-      let picture = document.querySelector('#spooky');
-      picture.setAttribute("src", obj[key]);
+    }, 2800);
+
+//9. Когда на серый div наводится указатель мыши, он удаляется из DOM. 
+  setTimeout(() => {
+    let deleteNow  = document.querySelector('#ghosting');
+    deleteNow.addEventListener("mouseover", removal, false);
+
+    function removal(e) {
+      deleteNow.remove();
     }
+  }, 3000);
 
+//10. Когда на оранжевый div наводится указатель мыши, его ширина удваивается. Когда мы уходим за пределы div, ширина возвращается к своему первоначальному размеру.
+  setTimeout(() => {
+    let increase = document.querySelector("#resize");
+    let size = increase.clientWidth;
 
+    increase.addEventListener("mouseover", () =>{
+      increase.style.width = size * 2 + "px"}, false);
 
+      increase.addEventListener("mouseout", () =>{ 
+        increase.style.width = size + "px" }, false);
+  }, 3000);
 
-     // #9 When the mouse pointer hovers over the gray div, it is removed from the DOM.
-    let ghost = document.querySelector("#ghosting");
-    ghost.addEventListener("mouseover",ghosting, false);
-    function ghosting() {
-      document.querySelector("body").removeChild(ghost);
-    }
-
-    // #10 When the mouse pointer hovers over the orange div, its width doubles. When we go beyond the div, the width returns to its original size
-    let width = document.querySelector("#resize");
-    width.addEventListener("mouseover",() => {width.style.width = "400px"}, false);
-    width.addEventListener("mouseout",() => {width.style.width = "200px"}, false);
-
-   // #11 When the Reset button is pressed, delete the selected class from each <li> and change the image to panic.jpg .
+//11. Когда  кнопка Reset нажата - удалите класс `selected` из каждого `<li>` и измените изображение на `panic.jpg`.
+  setTimeout(() => {
     let reset = document.querySelector("#reset")
-    reset.addEventListener("click",resetChanges, false);
-    function resetChanges () {
-      let selected = document.querySelectorAll(".selected");
-      selected.forEach(sel => sel.classList.remove('selected'))
-      let image = document.querySelector('#Spooky');
-      image.setAttribute("src", 'images/panic.jpg');
+    reset.addEventListener("click", delNew, false);
+    function delNew() {
+      let changeNow = document.querySelectorAll(".selected");
+      changeNow.forEach(del => del.classList.remove("selected"))
+      let picture = document.querySelector("#logo");
+      picture.setAttribute("src", "images/panic.jpg");
+  }
+  }, 3000);
+
+  //12. При нажатии клавиши 1, 2, 3, 4, 5, 6, 7, 8, 9 или 0 на странице появляется сообщение «Я НЕНАВИЖУ ЧИСЛААААА!»
+  setTimeout(() => {
+    window.addEventListener("keypress", nomderNot, false);
+    function nomderNot (e) {
+      if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) {
+        alert("«Я НЕНАВИЖУ ЧИСЛАААААА!»");
+      }
     }
-   // #12 When the key is pressed 1, 2, 3, 4, 5, 6, 7, 8, 9 or 0 the message "I HATE NUMBERS AAAAA!" appears on the page.
-    window.addEventListener("keydown", checkKeyPressed, false);
-    function checkKeyPressed(e) {
-      if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) 
-        alert("Я НЕНАВИЖУ ЧИСЛААААА!");    
-    }
-}, 1500);
+  }, 3000);
+
 })
